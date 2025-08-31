d2ee4ac259c31db47c97b3f1df1f42f4aaab3286
YX3527 <lawyxyx@gmail.com>
2025-08-31 04:57:06 +0000

    31/8

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,12 @@
-
+function setup() {
+    createCanvas(600, 400);
+    background(0);
+}
+function draw(){
+stroke("red")
+circle(100, 200, 150);
+fill(0, 0, 0)
+triangle(500, 100, 150, 200, 120, 220)
+fill(0, 0, 0)
+rect(100, 200, 150, 150)
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,9 @@
-
+function setup() {
+    createCanvas(600, 400);
+    background(220);
+}
+function draw(){
+    for(let i = 0; i < 10; i++)
+        circle (50 + i * 40, 90, 30);
+    
+}
